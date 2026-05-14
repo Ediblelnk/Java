8fe4f3b8cec991fa45f9984071beea5e2d7e2609
Ediblelnk <peters1731@gmail.com>
2026-05-13 23:58:10 -0400

    ch11 lint

--- apcsa/ch11/ParseInts.java
+++ apcsa/ch11/ParseInts.java
@@ -1,9 +1,10 @@
 package ch11;
+
 /**
  * ParseInts.java
- * 
+ *
  * Code Description: Reads a line of text and prints the integers in the line.
- * 
+ *
  * @author peters
  * @version 4/14/21
  * @contact 1009734@palisadessd.net
@@ -11,40 +12,39 @@ package ch11;
 
 import java.util.Scanner; //import Scanner class
 
-public class ParseInts
-{
-	/**
-	 * main method - Reads a line of text and prints the integers in the line.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		//declare variables for sum and current value
-		int iVal, iSum = 0;
-		
-		Scanner kb = new Scanner(System.in); //declare scanner instance
+public class ParseInts {
 
-		System.out.print("Enter a line of text: ");
-		Scanner scanLine = new Scanner(kb.nextLine());
+    /**
+     * main method - Reads a line of text and prints the integers in the line.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //declare variables for sum and current value
+        int iVal,
+            iSum = 0;
 
-		//scans the input for pure integers (no other characters in the term)
-		while (scanLine.hasNext())
-		{
-			try
-			{
-				//try to convert into an integer
-				iVal = Integer.parseInt(scanLine.next());
-				iSum += iVal;
-			}
-			catch(NumberFormatException exception) {}
-			//catch exception, but do nothing
-		}
+        Scanner kb = new Scanner(System.in); //declare scanner instance
 
-		//print the pure integer sum
-		System.out.println("\nThe sum of the integers on this line is: " + iSum);
-		
-		scanLine.close(); //close Scanner
-		kb.close(); //close Scanner
-	}
+        System.out.print("Enter a line of text: ");
+        Scanner scanLine = new Scanner(kb.nextLine());
+
+        //scans the input for pure integers (no other characters in the term)
+        while (scanLine.hasNext()) {
+            try {
+                //try to convert into an integer
+                iVal = Integer.parseInt(scanLine.next());
+                iSum += iVal;
+            } catch (NumberFormatException exception) {}
+            //catch exception, but do nothing
+        }
+
+        //print the pure integer sum
+        System.out.println(
+            "\nThe sum of the integers on this line is: " + iSum
+        );
+
+        scanLine.close(); //close Scanner
+        kb.close(); //close Scanner
+    }
 }
