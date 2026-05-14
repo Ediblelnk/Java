7ee065c3928426aec6463619c9b9eafc12b00ef5
Ediblelnk <peters1731@gmail.com>
2026-05-13 23:57:49 -0400

    ch8 lint

--- apcsa/ch8/Item.java
+++ apcsa/ch8/Item.java
@@ -1,9 +1,10 @@
 package ch8;
+
 /**
  * Item.java
- * 
+ *
  * Code Description: class definition for a shopping item
- * 
+ *
  * @author peters
  * @version 2/21/21
  * @contact 1009734@palisadessd.net
@@ -11,69 +12,69 @@ package ch8;
 
 import java.text.NumberFormat; //import number format class
 
-public class Item
-{
-	private String sName;
-	private double dPrice;
-	private int iQuantity;
-	
-	/**
+public class Item {
+
+    private String sName;
+    private double dPrice;
+    private int iQuantity;
+
+    /**
      * constructor - sets the name, price, and amount of an item
-     * 
+     *
      * @param
      */
-	public Item(String sItemName, double dItemPrice, int iNumPurchased)
-	{
-		sName = sItemName;
-	    dPrice = dItemPrice;
-	    iQuantity = iNumPurchased;
-	}
-	
-	/**
+    public Item(String sItemName, double dItemPrice, int iNumPurchased) {
+        sName = sItemName;
+        dPrice = dItemPrice;
+        iQuantity = iNumPurchased;
+    }
+
+    /**
      * getPrice - returns the price of an item
-     * 
+     *
      * @param
      */
-	public double getPrice()
-	{
-		return dPrice;
-	}
-	
-	/**
+    public double getPrice() {
+        return dPrice;
+    }
+
+    /**
      * getName - returns the name of an item
-     * 
+     *
      * @param
      */
-	public String getName()
-	{
-		return sName;
-	}
-	
-	/**
+    public String getName() {
+        return sName;
+    }
+
+    /**
      * getQuantity - returns the quantity of an item
-     * 
+     *
      * @param
      */
-	public int getQuantity()
-	{
-		return iQuantity; 
-	}
-	
-	/**
+    public int getQuantity() {
+        return iQuantity;
+    }
+
+    /**
      * toString - return a string with the information about the item
-     * 
+     *
      * @param
      */
-	public String toString ()
-	{
-		String sSpacing = "\t\t\t";
-		if(sName.length() > 7)
-			sSpacing = "\t\t";
-		if(sName.length() > 15)
-			sSpacing = "\t";
-		NumberFormat fmtCurrency = NumberFormat.getCurrencyInstance();
-		
-		return (sName + sSpacing + fmtCurrency.format(dPrice) + "\t\t" + iQuantity + "\t\t"
-	      + fmtCurrency.format(dPrice * iQuantity));
-	}
+    public String toString() {
+        String sSpacing = "\t\t\t";
+        if (sName.length() > 7) sSpacing = "\t\t";
+        if (sName.length() > 15) sSpacing = "\t";
+        NumberFormat fmtCurrency = NumberFormat.getCurrencyInstance();
+
+        return (
+            sName +
+            sSpacing +
+            fmtCurrency.format(dPrice) +
+            "\t\t" +
+            iQuantity +
+            "\t\t" +
+            fmtCurrency.format(dPrice * iQuantity)
+        );
+    }
 }
