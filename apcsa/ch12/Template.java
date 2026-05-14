fc4e0df0acb949af851fa1534cdb942c3ff03f2f
Ediblelnk <peters1731@gmail.com>
2026-05-13 23:58:17 -0400

    ch12 lint

--- apcsa/ch12/Template.java
+++ apcsa/ch12/Template.java
@@ -1,23 +1,21 @@
 package ch12;
+
 /**
  * Template.java
- * 
+ *
  * Code Description:
- * 
+ *
  * @author peters
- * @version 
+ * @version
  * @contact 1009734@palisadessd.net
  */
 
-public class Template
-{
-	/**
-	 * main method - 
-	 * 
-	 * @param args
-	 */
-	public static void main(String[ ] args)
-	{
-		
-	}
+public class Template {
+
+    /**
+     * main method -
+     *
+     * @param args
+     */
+    public static void main(String[] args) {}
 }
