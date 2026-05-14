9675325d2e122037ebbd880d37cfd4e29cb41c60
Ediblelnk <peters1731@gmail.com>
2026-05-13 23:56:59 -0400

    ch2 lint

--- apcsa/ch2/Template.java
+++ apcsa/ch2/Template.java
@@ -1,28 +1,28 @@
 package ch2;
+
 /**
  * Template.java
- * 
- * Code Description: 
- * 
+ *
+ * Code Description:
+ *
  * @author peters
- * @version 
+ * @version
  * @contact 1009734@palisadessd.net
  */
 
 import java.util.Scanner; //import Scanner class
 
-public class Template
-{
-  /**
-   * main method - 
-   * 
-   * @param args
-   */
-  public static void main(String[ ] args)
-  {
-	// starts the scanner
-    Scanner kb = new Scanner(System.in);
-    
-    kb.close(); // closing the Scanner object
-  }
+public class Template {
+
+    /**
+     * main method -
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // starts the scanner
+        Scanner kb = new Scanner(System.in);
+
+        kb.close(); // closing the Scanner object
+    }
 }
